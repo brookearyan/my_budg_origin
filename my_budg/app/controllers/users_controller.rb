76a93eb77d1848dd54843d01807344758f419350
github username <github email address>
2017-12-19 13:31:49 -0500

    adjusted css and new user

--- my_budg/app/controllers/users_controller.rb
+++ my_budg/app/controllers/users_controller.rb
@@ -17,12 +17,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      render json: @user, status: 201
       log_in @user
       flash[:success] = "welcome!"
-      render json: @user, status: 201
-      redirect_to user
+      redirect_to @user
     else
-      render 'new'
+      redirect_to home
     end
   end
 
